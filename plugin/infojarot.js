1b322c48921cd85b062ff9c8b476e3cd8da9c210
hendrastore <102216067+hendrastore@users.noreply.github.com>
2022-07-15 16:25:10 +0700

    Update and rename plugins/infojarot.js to plugin/infojarot.js

--- plugin/infojarot.js
+++ plugin/infojarot.js
@@ -1,28 +1,28 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
 conn.reply(m.chat, `
 ╭─˗ˏˋ *INFORMASI OWNER* ´ˎ˗
-│ ✎ _Nama_ : Jarot
-│ ✎ _Sekolah_ : Lulus
-│ ✎ _Umur_ : 17
-│ ✎ _Asal_ : Jawa timur
-│ ✎ _Status_ : Turu deck
+│ ✎ _Nama_ : Hendra
+│ ✎ _Hobi_ : Main ml
+│ ✎ _Umur_ : 16
+│ ✎ _Asal_ : Jawa bara
+│ ✎ _Status_ : Sibuk main ml
 │ ✎ _Official Grup 1_ :
 │    https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL
 │ ✎ _Official Grup 2_ :
-│    https://chat.whatsapp.com/HjRHck1G3WRHOx97fJkdMN
+│    
 │ ✎ _Youtube _:
-│    https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
+│    
 │ ✎ _Instagram_ : 
-│    https://instagram.com/oscarbotz_
+│    
 │ ✎ _WhatsApp_ :
-│    wa.me/6285850539404
+│    wa.me/6285794152433
 ╰───────────────────
 `.trim(), m)
 }
 
-handler.help = ['infojarot']
+handler.help = ['infoHendra']
 handler.tags = ['main', 'utama']
-handler.command = /^(infojarot)$/i
+handler.command = /^(infoHendra)$/i
 
 handler.exp = 150
 
