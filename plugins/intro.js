f061ceb04aeb8b8ed67d080c7620b758447cf391
ayoser2 <107592051+ayoser2@users.noreply.github.com>
2022-08-11 06:49:29 +0530

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -2,18 +2,18 @@ let handler = async m => {
 
 let krtu = `Kartu Intro`
 m.reply(`
-0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
-│       *「 Kartu Intro 」*
-│ *Nama     :* 
-│ *Gender   :* 
-│ *Umur      :* 
-│ *Hobby    :* 
-│ *Kelas      :* 
-│ *Asal         :* 
-│ *Agama    :* 
- |  *Status     :* 
-╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙
-`.trim()) // Tambah sendiri kalo mau
+0ཻུ۪۪ꦽꦼ̷⸙‹•💕═info💕═♡᭄
+│       *「 Owner Intro 」*
+│ *Name     :* Ayodya Vichakshana
+│ *Gender   :* Male
+│ *GF      :* No
+│ *Hobby    :* Like Github and Tech
+│ *Age      :* 15
+│ *Birthday         :* 8/8
+│ *Country    :* Sri lanka
+ |  *Status     :* Bot Online
+╰═════ꪶ ཻུ۪۪ꦽꦼ̷ཻུ۪۪ꦽꦼ̷ཻུ۪۪ꦽꦼ̷ཻུ۪۪۪۪۪۪۪ꦽꦼ̷ཻུ۪۪ꦽꦼ̷ཻུ۪۪ayodya ꪶ ཻུ۪۪ꦽꦼ̷⸙
+`.trim()) // Hi
 }
 handler.command = /^(intro)$/i
 
